47c3380beba35800530f1afa62883b6bb9048f03
Caia Chuang <caiachuang@gmail.com>
2020-09-16 22:12:34 -0700

    Make notes of KV's tutorial videos.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,16 @@
 class ProductsController < ApplicationController
 
   def index
+    # uses MODEL
     @products = Product.all.order(created_at: :desc)
+    # VIEW
+    #render :indes # implicit
   end
 
   def show
     @product = Product.find params[:id]
+    @msg = "Hellooooo"
+    render 'show'  # implicit
   end
 
 end
