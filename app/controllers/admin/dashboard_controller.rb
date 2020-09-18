adb0c1b30959e7168cb33856a92cd07bba03db19
Caia Chuang <caiachuang@gmail.com>
2020-09-17 22:23:36 -0700

    Add links to admin/products and admin/root.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["HTTP_BASIC_AUTHENTICATE_NAME"], password: ENV["HTTP_BASIC_AUTHENTICATE_PASSWORD"]
+  
   def show
     @product_count = Product.all.count
     @category_count = Category.all.count
