bb45e1a3025b61c4de12f86b5292554de3b4f148
Caia Chuang <caiachuang@gmail.com>
2020-09-21 23:38:35 -0700

    Create tests for authenticate_with_credentials method and correct authenticate_with_credentials method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,20 @@
 class UsersController < ApplicationController
   def new
-  end
+	end
 
-  def create
-    # user = User.new(user_params)
-    if  user = User.authenticate_with_credentials(params[:email], params[:password])
-      session[:user_id] = user.id
-      redirect_to '/'
-    else
-      redirect_to '/singnup'  # need? since no restrick from access
-    end
-  end
+	def create
+    user = User.new(user_params)
+    
+		if user.save
+			session[:user_id] = user.id
+			redirect_to '/'
+		else
+			redirect_to '/singnup'  # need? since no restrick from access
+		end
+	end
 
-  # private
-  # 	def user_params
-  # 		params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-  # 	end
+	private
+		def user_params
+			params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+		end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,54 +4,81 @@ RSpec.describe User, type: :model do
   describe "Validation" do
 
     it "should be create with 'password' and 'password_confirmation' fields" do
-      @user = User.new(first_name: "Test-first", last_name: "Test-last", email: "test@email.com", password: nil, password_confirmation: nil)
+      @user = User.new(first_name: "First", last_name: "Last", email: "test@email.com", password: nil, password_confirmation: nil)
       
       expect(@user.save).to be false
     end
     
     it "should have the same value for 'password' and 'password_confirmation' fields" do
-      @user = User.new(first_name: "Test-first", last_name: "Test-last", email: "test@email.com", password: "12345", password_confirmation: "12345")
+      @user = User.new(first_name: "First", last_name: "Last", email: "test@email.com", password: "12345", password_confirmation: "12345")
 
       expect(@user.password).to eq(@user.password_confirmation)
     end
 
     it "should not be saved if 'password' and 'password_confirmation' fields are not the same" do
-      @user = User.new(first_name: "Test-first", last_name: "Test-last", email: "test@email.com", password: "123", password_confirmation: "12345")
+      @user = User.new(first_name: "First", last_name: "Last", email: "test@email.com", password: "123456", password_confirmation: "12345")
       expect(@user.save).to be false
     end
 
     it "should input unique email" do
-      @user = User.new(first_name: "Test-first", last_name: "Test-last", email: "test@email.com", password: "1234", password_confirmation: "1234" )
+      @user = User.new(first_name: "First", last_name: "Last", email: "test@email.com", password: "123456", password_confirmation: "123456" )
       @user.save
-      @another = User.new(first_name: "Test-first2", last_name: "Test-last2", email: "TEST@EMAIL.COM", password: "12345", password_confirmation: "12345")
+      @another = User.new(first_name: "Second", last_name: "Last", email: "TEST@EMAIL.COM", password: "12345", password_confirmation: "12345")
     
       expect(@another.save).to be false
     end
 
-    it "should input at least 5 letters to email" do
-      @user = User.new(first_name: "Test-first", last_name: "Test-last", email: "test", password: "1234", password_confirmation: "1234")
+    it "should input at least 5 letters to password" do
+      @user = User.new(first_name: "First", last_name: "Last", email: "test@email.com", password: "1234", password_confirmation: "1234")
     
       expect(@user.save).to be false
     end
     
     it 'should require the first_name field' do
-      @user = User.new(first_name: nil, last_name: "Test-last", email: "test@email.com", password: "12345", password_confirmation: "12345")
+      @user = User.new(first_name: nil, last_name: "Last", email: "test@email.com", password: "12345", password_confirmation: "12345")
 
       expect(@user.save).to be false
     end
 
     it 'should require the last_name field' do
-      @user = User.new(first_name: "Test-first", last_name: nil, email: "test@email.com", password: "12345", password_confirmation: "12345")
+      @user = User.create(first_name: "First", last_name: nil, email: "test@email.com", password: "12345", password_confirmation: "12345")
 
       expect(@user.save).to be false
     end
 
     it 'should require the email field' do
-      @user = User.new(first_name: "Test-first", last_name: "Test-last", email: nil, password: "12345", password_confirmation: "12345")
+      @user = User.create(first_name: "First", last_name: "Last", email: nil, password: "12345", password_confirmation: "12345")
 
       expect(@user.save).to be false
     end
   end
 
-  
+  describe '.authenticate_with_credentials' do
+
+    it 'should return user if email and password are authenticated' do
+      @user = User.create(first_name: "First", last_name: "Last", email: "email@email.com", password: "12345", password_confirmation: "12345")
+      @authenticated_user = User.authenticate_with_credentials("email@email.com", "12345")
+      
+      expect(@user).to eq(@authenticated_user)
+    end
+
+    it 'should return nil if email and password are not authenticated' do
+      @user = User.create(first_name: "First", last_name: "Last", email: "test@email.com", password: "123", password_confirmation: "123")
+      @authenticated_user = User.authenticate_with_credentials(@user.email, "123")
+      expect(@authenticated_user).to be nil
+    end
+
+    it "should be authenticated successfully if a visitor types in a few spaces before and/or after their email address" do
+      @user = User.create(first_name: "First", last_name: "Last", email: "test@email.com", password: "12345", password_confirmation: "12345")
+      @authenticated_user = User.authenticate_with_credentials("  test@email.com ", "12345")
+      expect(@user).to eq(@authenticated_user)
+    end
+
+    it "should be authenticated successfully if a visitor types mixed cases their email address" do
+      @user = User.create(first_name: "First", last_name: "Last", email: "test@email.com", password: "12345", password_confirmation: "12345")
+      @authenticated_user = User.authenticate_with_credentials("tEst@eMail.com", "12345")
+      expect(@user).to eq(@authenticated_user)
+    end
+  end
+    
 end
