a0273151bd84946e21118b71ff34cfb161260418
Caia Chuang <caiachuang@gmail.com>
2020-09-22 18:12:43 -0700

    Create scenario for product detail page and pass it.

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js:true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+  
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+
+  end
+  
+  scenario "Go to product details page" do
+    visit '/'
+    first("article.product").click_on('Details')
+    expect(page).to have_css('div dl.dl-horizontal')
+    # save_screenshot
+    # puts page.html
+  end
+end
